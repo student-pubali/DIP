9acd7a7dc93caad4188ba7387012a6c9e9ca4491
Pubali Karan <146906061+student-pubali@users.noreply.github.com>
2024-12-16 11:38:03 +0530

    Add files via upload

---
+++ DIP8.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import random
+img = cv2.imread("low_cont.jpg",0)
+imgP = img.copy()
+imgSP = img.copy()
+h,w = img.shape[:2]
+rng = random.randint(h//4,h)
+for i in range(rng):
+    x1 = random.randint(0,h-1)
+    y1 = random.randint(0,w-1)
+    x2 = random.randint(0,h-1)
+    y2 = random.randint(0,w-1)
+    imgSP[x1][y1] = 255
+    imgSP[x2][y2] = 0
+
+cv2.imwrite("salt_pep.jpg",imgSP)    
