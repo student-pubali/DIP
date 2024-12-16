72a02bbeb212601975c01c63674a426377ba6ff4
Pubali Karan <146906061+student-pubali@users.noreply.github.com>
2024-12-16 12:07:04 +0530

    Add files via upload

---
+++ DIP10.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+def erotion(image,structEle,iter):
+    imgNew = image.copy()
+    for k in range(iter):
+        for i in range(2,h-2):
+            for j in range(2,w-2):
+                flag = False
+                for m in range(5):
+                    for n in range(5):
+                        if(structEle[m][n]==1)and(image[i-2+m][j-2+n]==255):
+                            flag = True
+                if(flag):
+                    imgNew[i][j]=255
+                else:
+                    imgNew[i][j]=0
+        image = imgNew.copy()
+    return imgNew
+img = cv2.imread("j.png",0) 
+imgNew = img.copy()
+structEle = [[1,1,1,1,1],
+             [1,1,1,1,1],
+             [1,1,1,1,1],
+             [1,1,1,1,1],
+             [1,1,1,1,1]] 
+h,w = img.shape[:2] 
+imgNew = erotion(img,structEle,3) 
+cv2.imwrite("dailate.png",imgNew)                            
